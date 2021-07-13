a2ff178abb2ee83c78114b82c2350765c4abd952
walleser-karli <k.walleser@ufl.edu>
2021-07-13 01:15:57 -0400

    Add new instance of IceCream called vanillaIceCream, invoke earlier method, and log the new object and method to the console

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ class Desserts {
  constructor (type, calories) {
   this.type = type;
   this.calories = calories;
+  console.log("yum");
  }
 }
 
@@ -11,4 +12,13 @@ class IceCream extends Desserts {
   this.flavor = flavor;
   this.scoops = scoops;
  }
+
+ includeSpoon() {
+  console.log("Here is your spoon!");
+ }
 }
+
+const vanillaIceCream = new IceCream("Ice Cream", 340, "Vanilla", 3);
+
+console.log(vanillaIceCream);
+vanillaIceCream.includeSpoon();
